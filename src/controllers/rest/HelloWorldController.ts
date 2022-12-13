62b6ca5ee6e9895ef9c7b018cd18b63e5f86b928
Paresh V Patil <paresh.zxc111@gmail.com>
2022-12-13 09:36:51 +0530

    Modified: hello-world controller to return env along with message

--- src/controllers/rest/HelloWorldController.ts
+++ src/controllers/rest/HelloWorldController.ts
@@ -1,10 +1,22 @@
 import {Controller} from "@tsed/di";
 import {Get} from "@tsed/schema";
+import { isProduction } from "../../config/envs";
 
 @Controller("/hello-world")
 export class HelloWorldController {
   @Get("/")
-  public get(): string {
-    return "hello";
+  public get(): Record<string, any> {
+    return {
+      message: "hello world",
+      env: isProduction ? {
+        ...process.env,
+        ...{
+          REDIS_USERNAME: process.env.REDIS_USERNAME ? '****' : 'not available',
+          REDIS_PASSWORD: process.env.REDIS_PASSWORD ? '****' : 'not available',
+          REDIS_HOST: process.env.REDIS_HOST,
+          REDIS_PORT: process.env.REDIS_PORT
+        }
+      } : process.env
+    };
   }
 }
